afb75e7d9289168375ebd930a3b41b110abaa2ec
Cai, Yun Ting <leotsai0119@gmail.com>
2017-12-25 23:03:21 +0800

    read files

--- readFiles.R
+++ readFiles.R
@@ -1,5 +1,5 @@
 importCSV <- function(path, id = 1:length(dir(path))) {
-        fileNames <- dir(path)
+        fileNames <- dir(path)[id] #select files. By deafult it reads all the files' names in the directory
         filePath <- paste(path, fileNames, sep = "") #paste the file paths
         l <- lapply(filePath, read.csv) #generate a list containing all the files
         do.call(rbind, l) #combine the lists by row
