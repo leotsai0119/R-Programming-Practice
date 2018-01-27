70171817c9341e6dff02bccbe30f2e1691f6a72d
Cai, Yun-Ting <leotsai0119@gmail.com>
2018-01-27 15:40:34 +0800

    Update sampling_Lottery.R

--- sampling_Lottery.R
+++ sampling_Lottery.R
@@ -18,3 +18,15 @@ TaiwanLottery <- function(n){
 
 #testing the funciton
 myLottery <- TaiwanLottery(1000)
+
+#matching
+jackpot <- c(05, 27, 29, 36, 40 ,42, 35)
+m <- apply(myLottery, 1, match, jackpot)
+m
+na <- !is.na(m)
+use <- apply(na, 2, sum) >= 3
+pick <- myLottery[which(use == TRUE), ]
+
+#test
+fruit <- c("apple", "banana", "pear", "pineapple")
+str_count(fruit, "a")
