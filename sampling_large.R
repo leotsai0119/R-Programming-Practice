cbf998abed6899dde5d136e55f1bbda54da1d4e1
Cai, Yun-Ting <leotsai0119@gmail.com>
2018-03-04 15:06:19 +0800

    sampling_large_0304

--- sampling_large.R
+++ sampling_large.R
@@ -1,10 +1,11 @@
-#vnilla sampling
+#vanilla sampling with for loop
 x <- 1:10000
 s1 <- list(NULL)
 m1 <- list(NULL)
 for(i in 1:50){
-      s1[[i]] <-  sample(x, 30, replace = FALSE)
+      s1[[i]] <-sample(x, 30, replace = FALSE)
       m1[[i]] <- mean(s1[[i]])
+      d1 <- as.data.frame(do.call(rbind, m1))
 }
 
-lapply(s1, mean)
+#vanilla function with for loop
