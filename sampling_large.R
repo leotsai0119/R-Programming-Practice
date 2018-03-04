7248e0ac0f4b7d892bc608e2d3739b4ac368ffa9
Cai, Yun-Ting <leotsai0119@gmail.com>
2018-03-04 15:45:54 +0800

    sampling_LLN_0304

--- sampling_large.R
+++ sampling_large.R
@@ -1,11 +1,31 @@
+rm(list = ls())
 #vanilla sampling with for loop
 x <- 1:10000
 s1 <- list(NULL)
 m1 <- list(NULL)
 for(i in 1:50){
-      s1[[i]] <-sample(x, 30, replace = FALSE)
+      s1[[i]] <- sample(x, 30, replace = FALSE)
       m1[[i]] <- mean(s1[[i]])
       d1 <- as.data.frame(do.call(rbind, m1))
 }
 
 #vanilla function with for loop
+population <- 1:10000
+LLN <- function(population, size, times){
+        s <- list(NULL)
+        m <- list(NULL)
+        for(i in 1:times){
+                s[[i]] <- sample(population, size, replace = FALSE)
+                m[[i]] <- mean(s[[i]])
+        }
+        d <- as.data.frame(do.call(rbind, m))
+        p <- ggplot2::qplot(d$V1, geom = "histogram", bins = 50)
+        l <- list("sample" = d, "histogram" = p)
+        return(l)
+}
+
+#test the function
+y <- LLN(population, 30, 1000)
+d1 <- y$sample
+p <- y$histogram
+
